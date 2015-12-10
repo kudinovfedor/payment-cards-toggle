d998af0e95d9b8cef7cedbc71619da30243332ba
Fedor Kudinov <fedor.kudinov.dvlopbiz@gmail.com>
2015-12-10 11:34:00 +0200

    Replaced attr() on prop()

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-'use strict';
-(function ($) {
+;(function ($) {
+
+  'use strict';
+
   $(function () {
 
     $("select").styler();
@@ -9,6 +11,8 @@
       idCardIcons = $('.credit-cards-icon'),
       cardClass = 'mc visa ae ec jcb';
 
+    changeSelect(idSelect, idCardImg);
+
     idSelect.on('change', function () {
       changeSelect(idSelect, idCardImg);
     });
@@ -17,27 +21,26 @@
       changeCardIcon($(this), idCardImg, idSelect);
     });
 
-    changeSelect(idSelect, idCardImg);
-
     function changeCardIcon(idCardIcons, idCardImg, idSelect) {
       var thisIcon = idCardIcons.attr('class');
       if (idCardImg.attr('class') !== thisIcon) {
         idCardImg.removeClass(cardClass).addClass(thisIcon);
         idSelect.find('option').each(function () {
-          if ($(this).attr('value') === thisIcon) {
-            idSelect.find('option').removeAttr('selected');
-            $(this).attr('selected', 'selected').trigger('refresh');
+          if ($(this).val() === thisIcon) {
+            idSelect.find('option').prop('selected', false);
+            $(this).prop('selected', true).trigger('refresh');
           }
         });
       }
     }
 
     function changeSelect(idSelect, idCardImg) {
-      var selectOptionValue = idSelect.find(':selected').attr('value');
+      var selectOptionValue = idSelect.find(':selected').val();
       if (idCardImg.attr('class') !== selectOptionValue) {
         idCardImg.removeClass(cardClass).addClass(selectOptionValue);
       }
     }
 
   });
+
 }(jQuery));
